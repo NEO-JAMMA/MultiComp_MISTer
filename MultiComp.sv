b1c229eba7b560b6697725dd3386ae48af3da044
Cyril Venditti <cvenditti@TMW.local>
2018-06-08 12:57:18 -0400

    Initial commit

---
+++ MultiComp.sv
@@ -0,0 +1,226 @@
+//============================================================================
+//  Grant’s multi computer
+// 
+//  Port to MiSTer.
+//
+//  Based on Grant’s multi computer
+//  http://searle.hostei.com/grant/
+//  http://searle.hostei.com/grant/Multicomp/index.html
+//	 and WiSo's collector blog (MiST port)
+//	 https://ws0.org/building-your-own-custom-computer-with-the-mist-fpga-board-part-1/
+//	 https://ws0.org/building-your-own-custom-computer-with-the-mist-fpga-board-part-2/
+//
+//  This program is free software; you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License as published by the Free
+//  Software Foundation; either version 2 of the License, or (at your option)
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//  more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//============================================================================
+
+
+module emu
+(
+	//Master input clock
+	input         CLK_50M,
+
+	//Async reset from top-level module.
+	//Can be used as initial reset.
+	input         RESET,
+
+	//Must be passed to hps_io module
+	inout  [44:0] HPS_BUS,
+
+	//Base video clock. Usually equals to CLK_SYS.
+	output        CLK_VIDEO,
+
+	//Multiple resolutions are supported using different CE_PIXEL rates.
+	//Must be based on CLK_VIDEO
+	output        CE_PIXEL,
+
+	//Video aspect ratio for HDMI. Most retro systems have ratio 4:3.
+	output  [7:0] VIDEO_ARX,
+	output  [7:0] VIDEO_ARY,
+
+	output  [7:0] VGA_R,
+	output  [7:0] VGA_G,
+	output  [7:0] VGA_B,
+	output        VGA_HS,
+	output        VGA_VS,
+	output        VGA_DE,    // = ~(VBlank | HBlank)
+
+	output        LED_USER,  // 1 - ON, 0 - OFF.
+
+	// b[1]: 0 - LED status is system status OR'd with b[0]
+	//       1 - LED status is controled solely by b[0]
+	// hint: supply 2'b00 to let the system control the LED.
+	output  [1:0] LED_POWER,
+	output  [1:0] LED_DISK,
+
+	output [15:0] AUDIO_L,
+	output [15:0] AUDIO_R,
+	output        AUDIO_S,   // 1 - signed audio samples, 0 - unsigned
+	output  [1:0] AUDIO_MIX, // 0 - no mix, 1 - 25%, 2 - 50%, 3 - 100% (mono)
+	input         TAPE_IN,
+
+	// SD-SPI
+	output        SD_SCK,
+	output        SD_MOSI,
+	input         SD_MISO,
+	output        SD_CS,
+	input         SD_CD,
+
+	//High latency DDR3 RAM interface
+	//Use for non-critical time purposes
+	output        DDRAM_CLK,
+	input         DDRAM_BUSY,
+	output  [7:0] DDRAM_BURSTCNT,
+	output [28:0] DDRAM_ADDR,
+	input  [63:0] DDRAM_DOUT,
+	input         DDRAM_DOUT_READY,
+	output        DDRAM_RD,
+	output [63:0] DDRAM_DIN,
+	output  [7:0] DDRAM_BE,
+	output        DDRAM_WE,
+
+	//SDRAM interface with lower latency
+	output        SDRAM_CLK,
+	output        SDRAM_CKE,
+	output [12:0] SDRAM_A,
+	output  [1:0] SDRAM_BA,
+	inout  [15:0] SDRAM_DQ,
+	output        SDRAM_DQML,
+	output        SDRAM_DQMH,
+	output        SDRAM_nCS,
+	output        SDRAM_nCAS,
+	output        SDRAM_nRAS,
+	output        SDRAM_nWE
+);
+
+assign {DDRAM_CLK, DDRAM_BURSTCNT, DDRAM_ADDR, DDRAM_DIN, DDRAM_BE, DDRAM_RD, DDRAM_WE} = 0;
+assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
+
+assign AUDIO_S   = 1;
+assign AUDIO_MIX = status[3:2];
+
+assign LED_USER  = 0;
+assign LED_DISK  = 0;
+assign LED_POWER = 0;
+
+assign VIDEO_ARX = status[5:4] ? 8'd16 : 8'd4;
+assign VIDEO_ARY = status[5:4] ? 8'd9  : 8'd3;
+
+localparam CONF_BDI   = "(BDI)";
+localparam CONF_PLUSD = "(+D) ";
+localparam CONF_PLUS3 = "(+3) ";
+
+`include "build_id.v"
+localparam CONF_STR = {
+	"MultiComp;;"
+};
+
+
+////////////////////   CLOCKS   ///////////////////
+
+assign CLK_VIDEO = clk_sys;
+
+wire locked;
+wire clk_sys;
+
+pll pll
+(
+	.refclk(CLK_50M),
+	.rst(0),
+	.outclk_0(clk_sys),
+	.outclk_1(SDRAM_CLK),
+	.locked(locked)
+);
+
+//////////////////   HPS I/O   ///////////////////
+wire [15:0] joy_0;
+wire [15:0] joy_1;
+wire [15:0] joya_0;
+wire [15:0] joya_1;
+wire  [1:0] buttons;
+wire [31:0] status;
+wire [24:0] ps2_mouse;
+
+wire PS2_CLK;
+wire PS2_DAT;
+
+wire forced_scandoubler;
+
+wire [31:0] sd_lba;
+wire        sd_rd;
+wire        sd_wr;
+wire        sd_ack;
+wire  [8:0] sd_buff_addr;
+wire  [7:0] sd_buff_dout;
+wire  [7:0] sd_buff_din;
+wire        sd_buff_wr;
+wire        img_mounted;
+wire        img_readonly;
+wire [63:0] img_size;
+wire        sd_ack_conf;
+
+hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
+(
+	.clk_sys(clk_sys),
+	.HPS_BUS(HPS_BUS),
+
+	.conf_str(CONF_STR),
+
+	.joystick_0(joy_0),
+	.joystick_1(joy_1),
+	.joystick_analog_0(joya_0),
+	.joystick_analog_1(joya_1),
+
+	.buttons(buttons),
+	.status(status),
+	.forced_scandoubler(forced_scandoubler),
+
+	.ps2_kbd_clk_out(PS2_CLK),
+	.ps2_kbd_data_out(PS2_DAT),
+	
+	.ps2_kbd_led_use(0),
+	.ps2_kbd_led_status(0),
+
+	.ps2_mouse(ps2_mouse),
+
+	.sd_lba(sd_lba),
+	.sd_rd(sd_rd),
+	.sd_wr(sd_wr),
+	.sd_ack(sd_ack),
+	.sd_ack_conf(sd_ack_conf),
+	.sd_buff_addr(sd_buff_addr),
+	.sd_buff_dout(sd_buff_dout),
+	.sd_buff_din(sd_buff_din),
+	.sd_buff_wr(sd_buff_wr),
+	.img_mounted(img_mounted),
+	.img_readonly(img_readonly),
+	.img_size(img_size),
+
+	.ioctl_wait(0)
+);
+
+Microcomputer Microcomputer(
+		.RESET_N(RESET),
+		.SDRAM_nCS(SDRAM_nCS),
+		.VGA_R(VGA_R),
+		.VGA_G(VGA_G),
+		.VGA_B(VGA_B),
+		.VGA_HS(VGA_HS),
+		.VGA_VS(VGA_VS),
+		.ps2_clk(PS2_CLK),
+		.ps2_data(PS2_DAT),
+);
+
+endmodule
+
